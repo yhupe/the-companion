c0c3efbab283f23c5dbcb05c1d4397c5589ca632
MightContainNuts <mightcontainnuts@icloud.com>
2025-02-17 15:43:31 +0100

    initial git

---
+++ application/views/whatsapp.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, request
+
+whatsapp = Blueprint("main", __name__)
+
+@whatsapp.route("/whatsapp_incoming", methods=["POST"])
+def whatsapp_incoming():
+    incoming_message = request.form.get("Body")
+    sender_number = request.form.get("From")
+    print(f"Received message: {incoming_message} from {sender_number}")
+    return '<Response><Message>Thank you for your message!</Message></Response>'
+
+
+@whatsapp.route("/whatsapp_status_callback", methods=["POST"])
+def whatsapp_status_callback():
+    status = request.form.get("MessageStatus")
+    message_sid = request.form.get("MessageSid")
+
+    print(f"Message SID: {message_sid}, Status: {status}")
+
+    return "", 200
